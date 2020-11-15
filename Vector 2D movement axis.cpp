b4eedbb1f3567f558017e645fc7d6837d86dd23b
Issashu Greybeard <73355148+issashu@users.noreply.github.com>
2020-11-15 11:14:35 +0200

    Update Vector 2D movement axis.cpp

--- Vector 2D movement axis.cpp
+++ Vector 2D movement axis.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+//Can also be done by creating classes for each Point and Vector of movement
+
 static vector <double> Velocity {0, 0};
 static vector <double> StartPosition {3.4 , 5.6};
 static vector <double> EndPosition {0.0 , 0.0};
@@ -11,16 +13,20 @@ int main()
   cout<<"Enter the velocity of movement:"<< endl;
   cin>>Velocity[0];
   cin>>Velocity[1];
-   
+  for (auto i : Velocity)
+    cout << i << " ";
+  cout << endl;
+
+     
   cout << "The new position will be:" << endl;
   EndPosition[0] = StartPosition[0]+Velocity[0];
   EndPosition[1] = StartPosition[1]+Velocity[1];
-  
-  /*DEBUG LINES BELOW: COMMENT OUT WHEN NOT USING
-  for (auto i : Velocity)
-    cout << i << " ";
-  cout<<endl<<endl;
   for (auto i : EndPosition)
     cout << i << " ";
-  */
+  cout << endl;
+
+  cout << "The distance travelled / lenght of the vector is: " << endl;
+  float Length = sqrt(Velocity[0]*Velocity[0]+Velocity[1]*Velocity[1]);
+
+  cout << Length << " units of distance"<<endl;             
 }
